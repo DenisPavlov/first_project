2e1b269c8b12a07dec8c1dacaebea12c149576f6
Денис Павлов <denis.pavlov@digital-ecosystems.ru>
2018-05-23 18:20:30 +0700

    в процессе чтения главы 4

--- src/main/scala/chapter4/Summer.scala
+++ src/main/scala/chapter4/Summer.scala
@@ -4,9 +4,12 @@ import ChecksumAccumulator.calculate
 
 object Summer {
   def main(args: Array[String]) = {
-    for (arg <- args)
-      println(arg + ": " + calculate(arg))
+//    for (arg <- args)
+//      println(arg + ": " + calculate(arg))
+    val s = s"Ответ равен ${6 * 7}"
+    print(s)
   }
+
 
 
 }
