01ce6f8554ddf3d4001c0a210a4f7f79f4a6b9e0
Денис Павлов <denis.pavlov@digital-ecosystems.ru>
2018-05-29 17:23:07 +0700

    scala

--- src/main/scala/chapter7/Match.scala
+++ src/main/scala/chapter7/Match.scala
@@ -1,8 +1,12 @@
-val firstArg = if (args.length > 0) args(0) else ""
+val firstArg = if (!args.isEmpty) args(0) else ""
 
-firstArg match {
-  case "salt" => println("pepper")
-  case "chips" => println("salsa")
-  case "eggs" => println("bacon")
-  case _ => println("huh?")
-}
+val friend =
+  firstArg match {
+    case "salt" => "pepper"
+    case "chips" => "salsa"
+    case "eggs" => "bacon"
+    case "hello" => 102
+    case _ => "huh?"
+  }
+
+println(friend)
